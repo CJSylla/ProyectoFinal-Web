b442666462c54e3871c09b0656e5d012b84bed63
Claude Junior Sylla <cjsylla93@gmail.com>
2019-04-23 20:48:28 -0400

    ......

---
+++ src/main/java/JSONTools/GeoLocation.java
@@ -0,0 +1,31 @@
+package JSONTools;
+
+public class GeoLocation {
+
+    // attributes
+    private String longitude;
+    private String latitude;
+
+    // Constructor
+    public GeoLocation(String longitude, String latitude){
+        this.setLongitude(longitude);
+        this.setLatitude(latitude);
+    }
+
+    // Getters & Setter
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}
---
+++ src/main/java/JSONTools/JSONUtil.java
@@ -0,0 +1,16 @@
+package JSONTools;
+
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+public class JSONUtil {
+
+    public static String toJSON(Object object){
+        return new Gson().toJson(object);
+    }
+
+    public static ResponseTransformer json(){
+        return JSONUtil::toJSON;
+    }
+
+}
